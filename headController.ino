58a5161acb6a43f5e0f4a58b27d360b26df8f127
Adam Collins <mr.adamcollins@gmail.com>
2013-12-13 14:16:11 +0000

    Working controller

--- headController.ino
+++ headController.ino
@@ -1,10 +1,10 @@
 #include <Servo.h> 
- 
+const boolean DEBUG = 0; 
 // Servo
 Servo headServo;         // Servo object from the servo library
 const int head_servo_pin = 9;  // Pin to connect the head servo to
 const int head_pot_pin   = 0;  // Pin to connect the pot that controls head direction
-const int pot_noise      = 2;  // Two consecutive readings will be considered equal if they are within this value
+const int pot_noise      = 10;  // Two consecutive readings will be considered equal if they are within this value
 int head_pot_val;        // Value of the head pot.
 
 // Buttons - Note: buttons are connected to pull-down resistors thus are nominally 0;
@@ -31,12 +31,12 @@ const int RIGHT_EYE_PIN  = 11;
 int left_eye_val;
 int right_eye_val;
 
-volatile boolean shine = true;
+
 void leftButtonISR()
 {
   if (millis() >= (last_left_time + debounce_milli))
   {
-    shine = !shine;
+    
      restart_routine_flag = true;
      cur_routine_index = LEFT_SHAKE;
      last_left_time = millis();
@@ -47,28 +47,21 @@ void rightButtonISR()
 {
   if (millis() >= (last_right_time + debounce_milli))
   {
-    shine = !shine;
+    
      restart_routine_flag = true;
      cur_routine_index = RIGHT_SHAKE;
      last_right_time = millis();
   } 
 }
 
-void setup() 
-{ 
-  Serial.begin(115200);
-  headServo.attach(9);  // attaches the servo on pin 9 to the servo object 
-  attachInterrupt(0, leftButtonISR, RISING);
-  attachInterrupt(1, rightButtonISR, RISING);
-  pinMode(LEFT_EYE_PIN, OUTPUT);
-  pinMode(RIGHT_EYE_PIN, OUTPUT);
-  head_pot_val = analogRead(head_pot_pin);
-} 
-
 boolean potValueChanged()
 {
   int new_head_pot_val = analogRead(head_pot_pin);
-
+  if (true) 
+  {
+    Serial.print(new_head_pot_val);
+    Serial.print(" ");
+  }
   // if current value is within pot_noise of last val return no change
   if (( new_head_pot_val <= (head_pot_val + pot_noise) ) && ( new_head_pot_val >= (head_pot_val - pot_noise) ))
   {
@@ -76,16 +69,48 @@ boolean potValueChanged()
   } else
   {
     head_pot_val = new_head_pot_val;
-    Serial.println("Pot Changed");
+    if (DEBUG) { Serial.println("Pot Changed"); }
     return true;
   }
 }
 
-void readPot()
+void setHeadFromPot()
 {
    int val = analogRead(head_pot_pin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val = map(val, 0, 1023, 5, 175);     // scale it to use it with the servo (value between 0 and 180) 
+  val = map(val, 0, 1023, 10, 170);     // scale it to use it with the servo (value between 0 and 180) 
+  Serial.println("====");
+  Serial.println(val);
   headServo.write(val);                  // sets the servo position according to the scaled value 
+}
+
+const int LOW_BRIGHTNESS = 10;
+const int HIGH_BRIGHTNESS = 100;
+const int NORMAL_FADE_AMOUNT = 1.0;
+const int FAST_FADE_AMOUNT = 10.0;
+volatile double fade_amount = 1.0;
+int fade_dir = 1;
+int brightness;
+
+void pulseEyes()
+{
+  if (DEBUG)
+  {
+  Serial.print(fade_amount);
+  Serial.print(" ");
+  Serial.print(fade_dir);
+  Serial.print(" ");
+  Serial.println(brightness);
+  }
+  // reverse the direction of the fading at the ends of the fade: 
+  if (( brightness <= LOW_BRIGHTNESS )  || ( brightness >= HIGH_BRIGHTNESS ))
+  {
+    fade_dir = -fade_dir;
+  }
+  
+  brightness = brightness + (fade_amount * fade_dir);
+  
+  analogWrite(LEFT_EYE_PIN, brightness);
+  analogWrite(RIGHT_EYE_PIN, brightness);
 }
 
 // Global variables for shake routine
@@ -94,24 +119,29 @@ int shake_left_bound = 10;
 int shake_right_bound = 170;
 int shake_speed = 1;
 int num_shakes = 0;
+const int MAX_LEFT_SHAKE = 10;
+const int MAX_RIGHT_SHAKE = 170;
 const int total_shakes = 5;
 const int shake_range = 20;
+
 void shakeHead(int shake_index)
 { 
   // check for restart
   if (restart_routine_flag == true)
   {
+    fade_amount = FAST_FADE_AMOUNT;
     num_shakes = 0;
+    
     if (shake_index == LEFT_SHAKE)
     {
-      shake_left_bound = 0;
-      shake_right_bound = 0 + shake_range;
-      cur_shake_val = 0 + shake_range/2;
+      shake_left_bound = MAX_LEFT_SHAKE;
+      shake_right_bound = MAX_LEFT_SHAKE + shake_range;
+      cur_shake_val = MAX_LEFT_SHAKE + shake_range/2;
     } else if (shake_index == RIGHT_SHAKE)
     {
-      shake_left_bound = 180 - shake_range;
-      shake_right_bound = 180;
-      cur_shake_val = 180 - shake_range/2;
+      shake_left_bound = MAX_RIGHT_SHAKE - shake_range;
+      shake_right_bound = MAX_RIGHT_SHAKE;
+      cur_shake_val = MAX_RIGHT_SHAKE - shake_range/2;
     }
     restart_routine_flag = false;
   }
@@ -123,6 +153,7 @@ void shakeHead(int shake_index)
   if (num_shakes >= total_shakes)
   {
     cur_routine_index = NORMAL;
+    fade_amount = NORMAL_FADE_AMOUNT;
   }
   if ((cur_shake_val >= shake_right_bound) || (cur_shake_val <= shake_left_bound))
   {
@@ -131,30 +162,42 @@ void shakeHead(int shake_index)
    num_shakes += 1;
   }    
 }
+
+void setup() 
+{ 
+  if (DEBUG) { Serial.begin(115200); }
+  
+  headServo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  attachInterrupt(0, leftButtonISR, RISING);
+  attachInterrupt(1, rightButtonISR, RISING);
+  pinMode(LEFT_EYE_PIN, OUTPUT);
+  pinMode(RIGHT_EYE_PIN, OUTPUT);
+  head_pot_val = analogRead(head_pot_pin);
+  brightness = LOW_BRIGHTNESS+1;
+  fade_dir = 1; // positive direction starting from low bightness + 1
+  fade_amount = NORMAL_FADE_AMOUNT;
+} 
  
+void normalBehaviour()
+{  
+  fade_amount = NORMAL_FADE_AMOUNT;   
+  setHeadFromPot(); 
+}
+
 void loop() 
 { 
   
   if ( potValueChanged() )
   {
-    cur_routine_index = NORMAL;
-    readPot(); 
+    cur_routine_index = NORMAL;    
+    normalBehaviour();
   }
-  Serial.println(cur_routine_index);
   if (cur_routine_index != NORMAL)
   { 
     shakeHead(cur_routine_index); // ASSUMES only 3 index values this will need to change if more are added
   } 
   
-  if (shine)
-  {
-    digitalWrite(LEFT_EYE_PIN, HIGH);
-    digitalWrite(RIGHT_EYE_PIN, HIGH);
-  } else
-  {
-    digitalWrite(LEFT_EYE_PIN, LOW);
-    digitalWrite(RIGHT_EYE_PIN, LOW);
-    
-  }
+  pulseEyes();
+  
   delay(15);                           // waits for the servo to get there 
 } 
